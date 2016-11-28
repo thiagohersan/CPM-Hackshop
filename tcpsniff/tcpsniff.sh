74637ce791c6d2d12c31dcd1c4725bfc709a7681
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-11-28 20:53:35 -0200

    tries to improve tcpsniff script

--- tcpsniff/tcpsniff.sh
+++ tcpsniff/tcpsniff.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pythonpid=`ps | grep -v awk | awk '/SimpleHTTPServer/{print $1}'`
+pythonpid=`ps | grep -v awk | awk '/python/{print $1}'`
 kill -9 $pythonpid
 
 python -m SimpleHTTPServer &
@@ -11,12 +11,15 @@ while [ $cnt -lt 10 ]
 do
   sudo tcpdump -i eth0 'port 80' -w out.cap &
   killpid=$!
-  sleep 10
+  sleep 30
   sudo kill -9 $killpid
   mkdir tmpFlowDir && cd tmpFlowDir
   tcpflow -r ../out.cap
+  sleep 1
   mkdir data
   foremost -i * -o data
+  sleep 1
+
   cd data
   for t in {gif,png,jpg} 
   do
@@ -25,17 +28,19 @@ do
       cp $t/* ../../imgs/
     fi
   done
+  sleep 1
 
-  cd ../../imgs
-  rm ../index.html
-  cp ../dump.html ../index.html
+  cd ../../
+  rm index.html
+  cp dump.html index.html
+  cd imgs/
 
   for file in `ls -t *`
   do
     sed -i.bak 's/<!--- XXX --->/<img src='\''\/imgs\/'$file\''>\'$'\n    &/g' ../index.html
   done
 
-  cd ..
+  cd ../
   sudo rm -rf *.bak tmpFlowDir out.cap
 
   let cnt=cnt+1
